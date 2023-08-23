c6fd4451d6805504d3976bf0610b7c7883f439a4
GT1000-9 <75435410+GT1000-9@users.noreply.github.com>
2023-08-23 08:19:08 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 #created the main python file
 import streamlit
-streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.title('My Parents New Healthy Diner')
+
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach, & Rocket Smoothie')
